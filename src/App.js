a17a60762f63ad48940626b734af006ae03fced6
DuongDEV16 <duong16042000@gmai.com>
2022-08-01 16:22:57 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Xuan Hoang</p>
+      <p></p>
+      p
     </div>
   );
 }
